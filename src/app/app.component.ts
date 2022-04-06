e058dfda32dfe2bd9721919c57094b6cff0794ff
Antonio Gomes <antoniogomes313@outlook.com>
2022-04-06 15:43:43 -0300

    Removendo item da lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,22 @@ export class AppComponent {
 
   constructor() {
     this.todos.push(new Todo(1, 'Learn Angular', false));
-    this.todos.push(new Todo(1, 'Learn F#', false));
-    this.todos.push(new Todo(1, 'Play Elden Ring', false));
+    this.todos.push(new Todo(2, 'Learn F#', true));
+    this.todos.push(new Todo(3, 'Play Elden Ring', false));
   }
+
+  remove(todo: Todo){
+    const index = this.todos.indexOf(todo);
+    if(index !== -1){
+      this.todos.splice(index, 1);
+    }
+}
+
+markAsDone(){
+    
+}
+
+markAsUndone(){
+    
+}
 }
