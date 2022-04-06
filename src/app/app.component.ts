cb3534e6efcb21006c6b2a6dfbd14fa1d0bde25d
Antonio Gomes <antoniogomes313@outlook.com>
2022-04-06 18:34:31 -0300

    Persistindo no Local Storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,12 +20,15 @@ export class AppComponent {
         Validators.required,
       ])]
     });
+
+    this.load();
   }
 
   add() {
     const title = this.form.controls['title'].value;
     const id = this.todos.length + 1;
     this.todos.push(new Todo(id, title, false));
+    this.save();
     this.clear();
   }
 
@@ -38,23 +41,31 @@ export class AppComponent {
     if(index !== -1){
       this.todos.splice(index, 1);
     }
+    this.save();
   }
 
   markAsDone(todo: Todo){
     todo.done = true;
+    this.save();
   }
 
   markAsUndone(todo: Todo){   
     todo.done = false;
+    this.save();
   }
 
-  processarLogin() {
-    if (this.form.invalid) {
-      return;
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    if (data) {
+      this.todos = JSON.parse(data);
+    } else {
+      this.todos = [];
     }
-    
-    const valoresFormulario = this.form.value;
-    console.log('Valores: ', valoresFormulario);
   }
 }
 
