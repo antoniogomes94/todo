bc4e27ca498ab31de949a0a757b965c92abf3d92
Antonio Gomes <antoniogomes313@outlook.com>
2022-04-06 16:16:55 -0300

    Mudança de status e form basico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.model';
 
 @Component({
@@ -9,8 +10,17 @@ import { Todo } from 'src/models/todo.model';
 export class AppComponent {
   public todos: Todo[] = [];
   public title: String = 'Minhas Tarefas';
+  public form!: FormGroup;
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+
     this.todos.push(new Todo(1, 'Learn Angular', false));
     this.todos.push(new Todo(2, 'Learn F#', true));
     this.todos.push(new Todo(3, 'Play Elden Ring', false));
@@ -21,13 +31,15 @@ export class AppComponent {
     if(index !== -1){
       this.todos.splice(index, 1);
     }
+  }
+
+  markAsDone(todo: Todo){
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo){   
+    todo.done = false;
+  }
 }
 
-markAsDone(){
-    
-}
 
-markAsUndone(){
-    
-}
-}
